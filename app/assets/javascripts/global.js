6a16b01784d82d3580dbdb2f3aeb241cfec18533
Tammy Shipps <mizziness@gmail.com>
2017-03-11 07:02:23 +0000

    JS tweaks

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,11 +1,12 @@
+/* global $ */
 
 $(document).ready( function() {
     
-    $('.panel').on('hide.bs.collapse', function (e) {
+    $('.panel-group').on('hide.bs.collapse', function (e) {
         $(".glyphicon.glyphicon-collapse-up").removeClass("glyphicon-collapse-up").addClass("glyphicon-collapse-down");
     });
     
-    $('.panel').on('show.bs.collapse', function (e) {
+    $('.panel-group').on('show.bs.collapse', function (e) {
         $(".glyphicon.glyphicon-collapse-down").removeClass("glyphicon-collapse-down").addClass("glyphicon-collapse-up");
     });
     
